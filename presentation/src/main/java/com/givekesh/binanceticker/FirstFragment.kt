c082a2dcd6e908ef87dce2b904307c786f2c28af
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-04-03 16:56:35 +0330

    add AndroidEntryPoint annotation for fragments and main activity in presentation module

--- presentation/src/main/java/com/givekesh/binanceticker/FirstFragment.kt
+++ presentation/src/main/java/com/givekesh/binanceticker/FirstFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.givekesh.binanceticker.databinding.FragmentFirstBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+@AndroidEntryPoint
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
