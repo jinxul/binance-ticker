3d1473c1ef064b5863c778618c2b9cb2642c84bf
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-04-04 02:35:09 +0330

    remove floating action button from main activity

--- presentation/src/main/java/com/givekesh/binanceticker/MainActivity.kt
+++ presentation/src/main/java/com/givekesh/binanceticker/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.givekesh.binanceticker
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -29,12 +28,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
