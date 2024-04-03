cd157a7e736c8bcf9fafb334d1936de630ff1543
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-04-03 16:10:25 +0330

    add bundle for presentation in version catalog

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -40,12 +40,8 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.bundles.presentation)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
