366f4095b505409cd3e43302d3a3a9342f638379
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-04-04 20:41:42 +0330

    remove number formatting in BindingAdapters.kt

--- presentation/src/main/java/com/givekesh/binanceticker/util/BindingAdapters.kt
+++ presentation/src/main/java/com/givekesh/binanceticker/util/BindingAdapters.kt
@@ -1,21 +1,16 @@
 package com.givekesh.binanceticker.util
 
 import androidx.core.content.ContextCompat
-import java.text.NumberFormat
 import androidx.databinding.BindingAdapter
 import com.givekesh.binanceticker.R
 import com.google.android.material.textview.MaterialTextView
-import java.util.Locale
 
 
 object BindingAdapters {
     @JvmStatic
     @BindingAdapter("setPrice")
-    fun setPriceTextAndColor(view: MaterialTextView, price: String) {
-        val formattedPrice = NumberFormat
-            .getNumberInstance(Locale.US)
-            .format(price.toDouble())
-        view.text = view.context.getString(R.string.price_in_usd, formattedPrice)
+    fun setPrice(view: MaterialTextView, price: String) {
+        view.text = view.context.getString(R.string.price_in_usd, price)
     }
 
     @JvmStatic
